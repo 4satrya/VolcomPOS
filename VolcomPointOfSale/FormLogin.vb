923f6f5dea397f5429dc6894faf1eb104ee98631
Satrya Anas <catur.satrya@gmail.com>
2019-11-04 10:58:10 +0800

    login tanpa try catch

--- VolcomPointOfSale/FormLogin.vb
+++ VolcomPointOfSale/FormLogin.vb
@@ -43,34 +43,34 @@ Public Class FormLogin
             Dim username As String = addSlashes(TxtUsername.Text)
             Dim password As String = addSlashes(TxtPassword.Text)
             Dim data As DataTable
-            Try
-                Cursor = Cursors.WaitCursor
-                Dim u As New ClassUser()
-                query = String.Format(u.queryMain("AND u.username='{0}' AND u.password=MD5('{1}')", "1"), username, password)
-                data = execute_query(query, -1, True, "", "", "", "")
-                If data.Rows.Count > 0 Then
-                    id_user = data.Rows(0)("id_user").ToString
-                    id_role_login = data.Rows(0)("id_role").ToString
-                    role_login = data.Rows(0)("role").ToString
-                    name_user = data.Rows(0)("employee_name").ToString
-                    username_user = data.Rows(0)("username").ToString
-                    id_employee_user = data.Rows(0)("id_employee").ToString
-                    is_change_pass_user = data.Rows(0)("is_change").ToString
-                    'reason
-                    If menu_acc = "20" And Not is_open_form Then
-                        FormPOS.id_user_employee_cancel = data.Rows(0)("id_employee").ToString
-                        FormPOS.note = MEReason.Text
-                    End If
-                    Opacity = 0
-                    Dispose()
-                    u.checkAccess(menu_acc, is_open_form)
-                Else
-                    XtraMessageBox.Show("Login failure, please check your input !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            'Try
+            Cursor = Cursors.WaitCursor
+            Dim u As New ClassUser()
+            query = String.Format(u.queryMain("AND u.username='{0}' AND u.password=MD5('{1}')", "1"), username, password)
+            data = execute_query(query, -1, True, "", "", "", "")
+            If data.Rows.Count > 0 Then
+                id_user = data.Rows(0)("id_user").ToString
+                id_role_login = data.Rows(0)("id_role").ToString
+                role_login = data.Rows(0)("role").ToString
+                name_user = data.Rows(0)("employee_name").ToString
+                username_user = data.Rows(0)("username").ToString
+                id_employee_user = data.Rows(0)("id_employee").ToString
+                is_change_pass_user = data.Rows(0)("is_change").ToString
+                'reason
+                If menu_acc = "20" And Not is_open_form Then
+                    FormPOS.id_user_employee_cancel = data.Rows(0)("id_employee").ToString
+                    FormPOS.note = MEReason.Text
                 End If
-                Cursor = Cursors.Default
-            Catch ex As Exception
-                stopCustom(ex.ToString)
-            End Try
+                Opacity = 0
+                Dispose()
+                u.checkAccess(menu_acc, is_open_form)
+            Else
+                XtraMessageBox.Show("Login failure, please check your input !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+            Cursor = Cursors.Default
+            'Catch ex As Exception
+            'stopCustom(ex.ToString)
+            'End Try
         End If
     End Sub
     'Validating Username
